90e43b27568732f8076d5a34947bd8a8f9e79201
Kevin Desktop <kevinluislima10@hotmail.com>
2026-06-29 20:21:35 -0300

    Revertidas mudanças que chocaram com outras partes do código

--- Server/serverbase/Server.java
+++ Server/serverbase/Server.java
@@ -3,22 +3,19 @@ package Server.serverbase;
 import Server.storage.FileStorageService;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Server {
-
     public static void main(String[] args) {
-
         try (ServerSocket serverSocket = new ServerSocket(ServerConfig.PORT)) {
 
             FileStorageService storageService = new FileStorageService(ServerConfig.STORAGE_PATH);
-            ExecutorService executorService = Executors.newFixedThreadPool(Math.max(8, Runtime.getRuntime().availableProcessors() * 2));
-            System.out.println("No de armazenamento iniciado na porta " + ServerConfig.PORT);
+
+            System.out.println("Nó iniciado na porta " + ServerConfig.PORT);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                executorService.submit(new ClientHandler(clientSocket, storageService));
+                Thread thread = new Thread(new ClientHandler(clientSocket, storageService));
+                thread.start();
             }
 
         } catch (Exception e) {
--- Server/serverbase/ServerConfig.java
+++ Server/serverbase/ServerConfig.java
@@ -6,6 +6,4 @@ public class ServerConfig {
 
     public static final int PORT = Integer.parseInt(Optional.ofNullable(EnvLoader.get("SERVER_PORT")).orElse("3000"));
     public static final String STORAGE_PATH = EnvLoader.get("STORAGE_PATH");
-    public static final String JWT_SECRET = EnvLoader.get("JWT_SECRET");
-
 }
