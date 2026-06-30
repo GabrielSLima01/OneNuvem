f1612c32419e91e403b9cad2870c6fc47489624d
Kevin Desktop <kevinluislima10@hotmail.com>
2026-06-30 19:21:27 -0300

    Adicionando controle de escalabilidade para o server (antes o server podia mandar um workflow infinito para o mesmo nó, agora ele terá uma quantidade limite e terá nós saudáveis numa fila de trabalho)

--- Server/serverbase/Server.java
+++ Server/serverbase/Server.java
@@ -3,22 +3,29 @@ package Server.serverbase;
 import Server.storage.FileStorageService;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     public static void main(String[] args) {
+        ExecutorService threadPool =
+                Executors.newFixedThreadPool(ServerConfig.WORKER_THREADS);
+
         try (ServerSocket serverSocket = new ServerSocket(ServerConfig.PORT)) {
 
             FileStorageService storageService = new FileStorageService(ServerConfig.STORAGE_PATH);
-            NodeLogger.info("server_start", "Nó iniciado na porta: " + ServerConfig.PORT);
+
+            NodeLogger.info("server_start", "Nó iniciado na porta " + ServerConfig.PORT + " com " + ServerConfig.WORKER_THREADS + " threads");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                Thread thread = new Thread(new ClientHandler(clientSocket, storageService));
-                thread.start();
+                threadPool.submit(new ClientHandler(clientSocket, storageService));
             }
 
         } catch (Exception e) {
-            NodeLogger.error("Server_fatal", "Falha ao iniciar o servidor: " + e.getMessage());
+            NodeLogger.error("server_fatal", "Falha ao iniciar o servidor: " + e.getMessage());
+        } finally {
+            threadPool.shutdown(); // libera as threads ao encerrar
         }
     }
 }
